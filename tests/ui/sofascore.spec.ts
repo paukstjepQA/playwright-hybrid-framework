8895d423e41d42cdd41837f0008890b1c88b3980
Stjepan Paukovac <paukstje00@gmail.com>
2026-05-10 21:11:05 +0200

    Lekcija 2: Dodana ekstrakcija i čišćenje podataka za trenera i stadion

--- tests/ui/sofascore.spec.ts
+++ tests/ui/sofascore.spec.ts
@@ -15,6 +15,20 @@ test('pretraga koristeći POM i Business Logic', async ({ page }) => {
     // 5. Završna provjera (Assertion)
     // Kada kliknemo, url se mora promijeniti u stranicu kluba
     await expect(page).toHaveURL(/.*2032.*/);
-    }
-);
+
+
+
+   // --- LEKCIJA 2: STRUKTURNE ASERCIJE --- provjera trenera i stadiona
+const trenerLink = page.locator('a[href*="/manager/"]').first();
+await expect(trenerLink).toBeVisible();
+let imeTrenera = await trenerLink.innerText();
+imeTrenera = imeTrenera.replace('Trener', '').replace('\n', '').trim();
+console.log(`Robot kaže: Trenutni trener Dinama je ${imeTrenera}`);
+
+const stadionLink = page.locator('a[href*="/venue/"]').first();
+await expect(stadionLink).toBeVisible();
+let imeStadiona = await stadionLink.innerText();
+console.log(`Robot kaže: Dinamo igra na stadionu ${imeStadiona}`);
+
+    });
     
