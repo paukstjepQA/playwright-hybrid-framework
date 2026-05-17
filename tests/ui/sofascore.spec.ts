b17356523bea69ce33806e705dca925ad81de9ab
Stjepan Paukovac <paukstje00@gmail.com>
2026-05-17 20:37:16 +0200

    Lekcija 3: Implementiran Data-Driven Testing za više klubova

--- tests/ui/sofascore.spec.ts
+++ tests/ui/sofascore.spec.ts
@@ -1,34 +1,32 @@
 import { test, expect } from '@playwright/test';
-import { SofaMainPage } from '../../pages/SofaMainPage'; 
+import { SofaMainPage } from '../../pages/SofaMainPage';
+const testniKlubovi = [
+    { imeKluba: 'Dinamo', klubId: '2032' },
+    { imeKluba: 'Rijeka', klubId: '2039' },
+    { imeKluba: 'Real Madrid', klubId: '2829' }
+ ]; // baza za više klubova (niz objekata)
 
-test('pretraga koristeći POM i Business Logic', async ({ page }) => {
-    const sofaMainPage = new SofaMainPage(page);
+    for (const klub of testniKlubovi) { // petlja koja vrti testove
+        test(`Provjera trenera i stadiona za: ${klub.imeKluba}`, async ({ page }) => {
+        const sofaMainPage = new SofaMainPage(page);
 
-    await sofaMainPage.navigate();
-    await sofaMainPage.acceptCookies();
-    await sofaMainPage.closeLanguageModal();
+        await sofaMainPage.navigate();
+        await sofaMainPage.acceptCookies();
+        await sofaMainPage.closeLanguageModal();
+        await sofaMainPage.searchFor(klub.imeKluba);
+        await sofaMainPage.clickResultById(klub.klubId);
+        await expect(page).toHaveURL(new RegExp(`.*${klub.klubId}.*`)); // dinamička provjera ID-a
 
-    await sofaMainPage.searchFor('Dinamo');
+        const trenerLink = page.locator('a[href*="/manager/"]').first(); // provjera trenera
+        await expect(trenerLink).toBeVisible();
+        let imeTrenera = await trenerLink.innerText();
+        imeTrenera = imeTrenera.replace('Trener', '').replace('\n', '').trim();
+        console.log(`🤖 Robot kaže: Trenutni trener kluba ${klub.imeKluba} je ${imeTrenera}`);
 
-    await sofaMainPage.clickResultById('2032');
-    
-    // 5. Završna provjera (Assertion)
-    // Kada kliknemo, url se mora promijeniti u stranicu kluba
-    await expect(page).toHaveURL(/.*2032.*/);
-
-
-
-   // --- LEKCIJA 2: STRUKTURNE ASERCIJE --- provjera trenera i stadiona
-const trenerLink = page.locator('a[href*="/manager/"]').first();
-await expect(trenerLink).toBeVisible();
-let imeTrenera = await trenerLink.innerText();
-imeTrenera = imeTrenera.replace('Trener', '').replace('\n', '').trim();
-console.log(`Robot kaže: Trenutni trener Dinama je ${imeTrenera}`);
-
-const stadionLink = page.locator('a[href*="/venue/"]').first();
-await expect(stadionLink).toBeVisible();
-let imeStadiona = await stadionLink.innerText();
-console.log(`Robot kaže: Dinamo igra na stadionu ${imeStadiona}`);
-
+        const stadionLink = page.locator('a[href*="/venue/"]').first(); // provjera stadiona
+        await expect(stadionLink).toBeVisible();
+        let imeStadiona = await stadionLink.innerText();
+        console.log(`🤖 Robot kaže: ${klub.imeKluba} igra na stadionu ${imeStadiona}`);
     });
-    
+
+}
